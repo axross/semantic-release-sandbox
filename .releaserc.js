ac74f1a83a4d91dd4bac8d4751f791287416a481
Kohei Asai <yo@kohei.dev>
2021-05-03 16:42:34 -0700

    feat: initial commit

---
+++ .releaserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: [
+    { name: "main" },
+    { name: "next", channel: "next", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/exec",
+      {
+        successCmd:
+          "echo ::set-output name=RELEASE_VERSION::${nextRelease.version}",
+      },
+    ],
+  ],
+};
