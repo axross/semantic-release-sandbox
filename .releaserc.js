d32d239b94905cdbb84da7fe6a980e727eaa94a7
Kohei Asai <yo@kohei.dev>
2021-05-03 17:45:55 -0700

    feat: update releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -4,7 +4,12 @@ module.exports = {
     { name: "next", channel: "next", prerelease: true },
   ],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
